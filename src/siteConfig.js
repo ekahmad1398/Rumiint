ac2aa62a83338929145859b4c3f9834b7177165d
ekahmad1398 <a78250006437@gmail.com>
2026-05-21 20:03:13 +0430

    sitemap updated

--- src/siteConfig.js
+++ src/siteConfig.js
@@ -1,15 +1,16 @@
 export const routes = Object.freeze({
-  home: '/',
-  about: '/about',
-  gallery: '/photos',
-  legacyGallery: '/gallery',
-  events: '/events',
-  legacyEvents: '/news',
-  faq: '/faq',
-  legacyPrograms: '/programs',
+    home: '/',
+    about: '/about',
+    gallery: '/photos',
+    legacyGallery: '/gallery',
+    events: '/events',
+    legacyEvents: '/news',
+    faq: '/faq',
+    legacyPrograms: '/programs',
 })
 
 export const siteDomain = 'rumi.edu.af'
+export const portalname = 'rumiint.com'
 export const siteUrl = `https://${siteDomain}`
-export const portalUrl = `https://portal.${siteDomain}`
+export const portalUrl = `https://${portalname}`
 export const contactEmail = `info@${siteDomain}`
